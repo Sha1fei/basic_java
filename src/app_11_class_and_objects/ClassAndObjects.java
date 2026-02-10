982f557c4d248e88fc8af5f4d248bc18cbe3da34
valentinzadorozhniy <someadress3000@gmail.com>
2026-02-10 11:37:08 +0700

    add variables info

--- src/app_11_class_and_objects/ClassAndObjects.java
+++ src/app_11_class_and_objects/ClassAndObjects.java
@@ -6,6 +6,14 @@ public class ClassAndObjects {
         System.out.println(computer.ram);
         System.out.println(computer.ssd);
         computer.load();
+        
+        // Получение системных свойств Java
+        // Можно добавить в VMOptions: -Dtest=test
+        // Или передать свойства через аргументы: arg1, arg2
+        var systemVariables = System.getProperties();
+        System.out.println("\nСистемные свойства Java:");
+        System.out.println("Java версия: " + systemVariables.getProperty("java.version"));
+        System.out.println("ОС: " + systemVariables.getProperty("os.name"));
     }
 }
 
