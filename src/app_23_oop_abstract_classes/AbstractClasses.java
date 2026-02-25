fc5c1a921a72a424b343fc60927afe76f56cf5a1
valentinzadorozhniy <someadress3000@gmail.com>
2026-02-25 18:47:17 +0700

    add abstarct classes

--- src/app_23_oop_abstract_classes/AbstractClasses.java
+++ src/app_23_oop_abstract_classes/AbstractClasses.java
@@ -2,28 +2,36 @@ package app_23_oop_abstract_classes;
 
 public class AbstractClasses {
     public static void main(String[] args) {
-        Cat cat = new Cat();
+        // Нельзя создать экземпляр абстрактного класса Animal, только его наследников.
+        Animal cat = new Cat();
         cat.setName("Murzik");
         System.out.println(cat.getName());
     }
 }
 
 abstract class Animal {
-    String name;
+    // protected: доступно наследникам, но скрыто от внешнего кода
+    protected String name;
 
+    // Общий геттер для всех наследников
     public String getName() {
         return name;
     }
 
-    ;
-
+    // Абстрактный сеттер: каждая реализация решает, как хранить имя
     public abstract String setName(String name);
 }
 
 class Cat extends Animal {
 
+    // Для кота сохраняем имя с простой валидацией
     @Override
     public String setName(String name) {
-        return this.name = name;
+        if (name == null || name.isBlank()) {
+            this.name = "Без имени";
+            return this.name;
+        }
+        this.name = name.trim();
+        return this.name;
     }
 }
