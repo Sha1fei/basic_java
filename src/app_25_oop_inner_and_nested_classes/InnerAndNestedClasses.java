3ce7f541ef026e8c8b2b2991167c14766bd62d4b
valentinzadorozhniy <someadress3000@gmail.com>
2026-02-28 10:48:35 +0700

    add inner and nested classes

--- src/app_25_oop_inner_and_nested_classes/InnerAndNestedClasses.java
+++ src/app_25_oop_inner_and_nested_classes/InnerAndNestedClasses.java
@@ -1,7 +1,8 @@
 package app_25_oop_inner_and_nested_classes;
 
 public class InnerAndNestedClasses {
-    String value;
+    // Поле внешнего класса, доступно внутренним классам
+    private final String value;
 
     public InnerAndNestedClasses(String value) {
         this.value = value;
@@ -11,14 +12,17 @@ public class InnerAndNestedClasses {
         InnerAndNestedClasses app = new InnerAndNestedClasses("test");
         app.handlerMethod();
 
+        // Нестатический внутренний класс создаётся через экземпляр внешнего класса
         InnerAndNestedClasses.InnerClass innerClass = app.new InnerClass();
         innerClass.testInner();
 
+        // Статический вложенный класс создаётся без экземпляра внешнего класса
         InnerAndNestedClasses.InnerClass2 innerClass2 = new InnerAndNestedClasses.InnerClass2();
         innerClass2.testInner2();
 
     }
 
+    // Метод демонстрирует локальный класс (объявляется внутри метода)
     void handlerMethod() {
         class NestedClass2 {
             void testInner() {
@@ -30,13 +34,14 @@ public class InnerAndNestedClasses {
         nestedClass2.testInner();
     }
 
+    // Статический вложенный класс: не имеет ссылки на внешний объект
     static class InnerClass2 {
         void testInner2() {
             System.out.println("run Nested Class2");
         }
     }
 
-
+    // Нестатический внутренний класс: имеет доступ к полям внешнего объекта
     class InnerClass {
         void testInner() {
             System.out.println("run Inner Class: " + InnerAndNestedClasses.this.value);
